f20546c08bf32dccfdc496588f005d0f6d5879af
Jason <47071224+JasonHalvorson@users.noreply.github.com>
2021-12-13 00:36:59 -0800

    Update TailwindCSS to 3.0.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,29 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-    mode     : 'jit',
-    purge    : [ './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}' ],
-    darkMode : 'class', // or 'media' or 'class'
-    theme    : {
+    content : [ './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}' ],
+    theme   : {
         colors : {
-            jhdark   : '#2D4D59',
+            jhdark   : '#071E3D',
             jhlight  : '#EDE1DF',
             jhblue   : '#41C4DD',
             jhpurple : '#9853A1',
-            jhgreen  : '#05EF9C'
+            jhgreen  : '#05EF9C',
+            black    : colors.black,
+            white    : colors.white,
+            gray     : colors.slate,
+            red      : colors.red,
+            orange   : colors.orange,
+            yellow   : colors.yellow,
+            green    : colors.green,
+            blue     : colors.blue,
+            indigo   : colors.indigo,
+            purple   : colors.purple,
+            pink     : colors.pink,
+            teal     : colors.teal,
+            cyan     : colors.cyan
         },
         extend : {}
     },
-    variants : {
-        extend : {}
-    },
-    plugins  : []
+    plugins : []
 };
