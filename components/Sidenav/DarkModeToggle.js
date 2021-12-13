3d850afa3b0d43bdbba6abcf6f99eb3d159546c5
Jason <47071224+JasonHalvorson@users.noreply.github.com>
2021-12-13 00:42:16 -0800

    Adjust darkmode switch colours

--- components/Sidenav/DarkModeToggle.js
+++ components/Sidenav/DarkModeToggle.js
@@ -28,7 +28,7 @@ export default function DarkModeToggle() {
     );
 
     return (
-        <Switch checked={darkMode} onChange={setDarkMode} className={classNames(darkMode ? 'bg-jhblue' : 'bg-gray-800', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-jhblue')}>
+        <Switch checked={darkMode} onChange={setDarkMode} className={classNames(darkMode ? 'bg-gray-900' : 'bg-gray-400', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-jhpurple')}>
             <span className="sr-only">Use setting</span>
             <span className={classNames(darkMode ? 'translate-x-5' : 'translate-x-0', 'pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200')}>
                 <span className={classNames(darkMode ? 'opacity-0 ease-out duration-100' : 'opacity-100 ease-in duration-200', 'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity text-orange-500')} aria-hidden="true">
