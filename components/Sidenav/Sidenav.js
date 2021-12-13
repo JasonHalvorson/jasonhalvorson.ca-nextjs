68707075c17fe703c073209144b22024edd7c459
Jason <47071224+JasonHalvorson@users.noreply.github.com>
2021-12-13 15:47:23 -0800

    Add transition to name

--- components/Sidenav/Sidenav.js
+++ components/Sidenav/Sidenav.js
@@ -33,7 +33,7 @@ export default function Sidenav(props) {
                             <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
                                 <div className="flex-shrink-0 flex items-center px-4">
                                     <img className="h-8 w-auto" src="/logo.png" alt="JH Logo" />
-                                    <div className="flex-1 ml-4 text-bold dark:text-white text-gray-900 text-xl">Jason Halvorson</div>
+                                    <div className="flex-1 ml-4 text-bold dark:text-white text-gray-900 text-xl transition-colors duration-300">Jason Halvorson</div>
                                 </div>
                                 <nav className="mt-5 px-2 space-y-1">
                                     {navigation.map((item) => (
@@ -61,7 +61,7 @@ export default function Sidenav(props) {
                     <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
                         <div className="flex items-center flex-shrink-0 px-4">
                             <img className="h-8 w-auto" src="/logo.png" alt="JH Logo" />
-                            <div className="flex-1 ml-4 text-bold dark:text-white text-gray-900 text-xl">Jason Halvorson</div>
+                            <div className="flex-1 ml-4 text-bold dark:text-white text-gray-900 text-xl transition-colors duration-300">Jason Halvorson</div>
                         </div>
                         <nav className="mt-5 flex-1 px-2 space-y-1">
                             {navigation.map((item) => (
