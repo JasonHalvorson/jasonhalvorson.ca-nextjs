306387a367da04d095ef8949b256f3b7170e5fc1
Jason <47071224+JasonHalvorson@users.noreply.github.com>
2021-12-16 01:37:40 -0800

    Regret.
    It took me 4 hours to realize that the reason for the FOUC was because I wasn't wrapping my portfolio pieces in a <Link> tag.

--- pages/portfolio/[piece].js
+++ pages/portfolio/[piece].js
@@ -1,16 +1,17 @@
 import Head from 'next/head';
 import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
-import { portfolioItems } from '../../lib/portfolio';
+import { getAllPieceSlugs, getPieceData } from '../../lib/portfolio';
 
 // FIXME: this page is using server-side rendering, and breaking dark mode
+// Dark mode is working on this page, but still causes a flash of light mode beforehand.
 
-export default function PortfolioItem({ currentItem }) {
+export default function PortfolioItem({ pieceData }) {
     // const router = useRouter();
     // const currentSlug = router.query.portfolioItem;
-    console.log(currentItem);
+    console.log(pieceData);
 
     // // check if slug matches any of the portfolio items slugs
-    // const currentItem = portfolioItems.find((item) => item.slug === currentSlug);
+    // const pieceData = portfolioItems.find((item) => item.slug === currentSlug);
 
     return (
         <div className="dark:bg-gray-900 bg-gray-100 transition-colors duration-300">
@@ -19,9 +20,9 @@ export default function PortfolioItem({ currentItem }) {
             </Head>
 
             <Breadcrumbs />
-            <p>{currentItem.title}</p>
-            <p>{currentItem.description}</p>
-            {currentItem.tags.map((tag) => (
+            <p>{pieceData.title}</p>
+            <p>{pieceData.description}</p>
+            {pieceData.tags.map((tag) => (
                 <span key={tag} className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-jhpurple text-white mr-1">
                     {tag}
                 </span>
@@ -30,19 +31,19 @@ export default function PortfolioItem({ currentItem }) {
     );
 }
 
-export async function getStaticProps(context) {
-    const currentItem = portfolioItems.find((item) => item.slug === context.params.portfolioItem);
-
-    return {
-        props : { currentItem }
-    };
-}
-
 export async function getStaticPaths() {
-    const paths = portfolioItems.map((item) => ({ params: { portfolioItem: item.slug } }));
+    const paths = getAllPieceSlugs();
 
     return {
         paths,
         fallback : false
+    };
+}
+
+export async function getStaticProps({ params }) {
+    const pieceData = getPieceData(params.piece);
+
+    return {
+        props : { pieceData }
     };
 }
--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,16 +1,26 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
-import { portfolioItems } from '../../lib/portfolio';
+import { getSortedPiecesData } from '../../lib/portfolio';
 
-export default function Portfolio() {
+export async function getStaticProps() {
+    const allPiecesData = getSortedPiecesData();
+
+    return {
+        props : {
+            allPiecesData
+        }
+    };
+}
+
+export default function Portfolio({ allPiecesData }) {
     return (
         <div className="dark:bg-gray-900 bg-gray-100 transition-colors duration-300">
             <Head>
                 <title>Jason Halvorson | Portfolio</title>
             </Head>
 
-            <Breadcrumbs />
+            {/* <Breadcrumbs /> */}
             <div className="relative max-w-7xl mx-auto">
                 <div className="max-w-7xl mx-auto py-8 px-4 sm:py-8 sm:px-6 lg:px-8">
                     <div className="text-center">
@@ -18,23 +28,25 @@ export default function Portfolio() {
                     </div>
                 </div>
                 <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-                    {portfolioItems.map((item) => (
-                        <div key={item.slug} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                    {allPiecesData.map((piece) => (
+                        <div key={piece.slug} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
                             <div className="flex-shrink-0">
-                                <img className="h-48 w-full object-cover" src={item.image} alt="" />
+                                <img className="h-48 w-full object-cover" src={piece.image} alt="" />
                             </div>
                             <div className="flex-1 dark:bg-gray-800 bg-white p-6 flex flex-col justify-between transition-colors duration-300">
-                                <div className="flex-1 items-center">
-                                    {item.tags.map((tag) => (
-                                        <span key={tag} className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-jhpurple text-white mr-1">
+                                <div className="flex-1 pieces-center">
+                                    {piece.tags.map((tag) => (
+                                        <span key={tag} className="inline-flex pieces-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-jhpurple text-white mr-1">
                                             {tag}
                                         </span>
                                     ))}
                                     {/* TODO: make portfolio  */}
-                                    <a href={`/portfolio/${item.slug}`} className="block mt-2">
-                                        <p className="text-xl font-semibold dark:text-white text-gray-900 transition-colors duration-300">{item.title}</p>
-                                        <p className="mt-3 text-base dark:text-gray-400 text-gray-500 transition-colors duration-300">{item.description}</p>
-                                    </a>
+                                    <Link href={`/portfolio/${piece.slug}`}>
+                                        <a className="block mt-2">
+                                            <p className="text-xl font-semibold dark:text-white text-gray-900 transition-colors duration-300">{piece.title}</p>
+                                            <p className="mt-3 text-base dark:text-gray-400 text-gray-500 transition-colors duration-300">{piece.description}</p>
+                                        </a>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
