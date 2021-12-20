9e6fa8e1996012ad8d1409badc7c01a50a2d1b1f
Jason <47071224+JasonHalvorson@users.noreply.github.com>
2021-12-20 01:10:23 -0800

    "Finish" portfolio page

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -20,23 +20,22 @@ export default function Portfolio({ allPiecesData }) {
                 <title>Jason Halvorson | Portfolio</title>
             </Head>
 
-            {/* <Breadcrumbs /> */}
-            <div className="relative max-w-7xl mx-auto">
+            <Breadcrumbs />
+            <div className="relative max-w-7xl mx-auto px-4">
                 <div className="max-w-7xl mx-auto py-8 px-4 sm:py-8 sm:px-6 lg:px-8">
                     <div className="text-center">
                         <p className="mt-1 text-4xl font-extrabold dark:text-white text-gray-900 sm:text-5xl transition-colors duration-300">My Portfolio</p>
                     </div>
                 </div>
-                <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+                <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-2 xl:grid-cols-3 lg:max-w-none">
                     {allPiecesData.map((piece) => (
                         <div key={piece.slug} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
                             <div className="flex-shrink-0">
-                                <img className="h-48 w-full object-cover" src={piece.image} alt="" />
+                                <img className="h-48 w-full object-cover" src={`/img/portfolio/${piece.image}`} alt="" />
                             </div>
                             <div className="flex-1 dark:bg-gray-800 bg-white p-6 flex flex-col justify-between transition-colors duration-300">
                                 <div className="flex-1 pieces-center">
                                     {piece.tags.map((tag) => (
-                                        <span key={tag} className="inline-flex pieces-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-jhpurple text-white mr-1">
                                         <span key={tag} className="inline-flex pieces-center px-3 py-0.5 rounded-full text-sm font-medium bg-jhpurple text-white mr-1">
                                             {tag}
                                         </span>
