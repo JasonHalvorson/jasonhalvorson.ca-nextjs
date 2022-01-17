43859c1ea0b60cff7c2a09768f68e8fbf39a1c45
Jason <47071224+JasonHalvorson@users.noreply.github.com>
2022-01-16 23:30:21 -0800

    Change blurb

--- pages/index.js
+++ pages/index.js
@@ -4,35 +4,35 @@ import { JavascriptIcon, ReactIcon, NextjsIcon, NodejsIcon, PythonIcon, CssIcon,
 
 const technologies = [
     {
-        name        : 'JavaScript, React, Next.js, Node.js',
-        icons       : [ JavascriptIcon, ReactIcon, NextjsIcon, NodejsIcon ],
-        description : 'Knowledgeable in JavaScript and JavaScript frameworks/libraries such as React, Next.js, and Node.js.'
+        name: 'JavaScript, React, Next.js, Node.js',
+        icons: [JavascriptIcon, ReactIcon, NextjsIcon, NodejsIcon],
+        description: 'Knowledgeable in JavaScript and JavaScript frameworks/libraries such as React, Next.js, and Node.js.',
     },
     {
-        name        : 'Python',
-        icons       : [ PythonIcon ],
-        description : 'Basic understanding of the Python language. Currently learning more via experimentation and online courses!'
+        name: 'Python',
+        icons: [PythonIcon],
+        description: 'Basic understanding of the Python language. Currently learning more via experimentation and online courses!',
     },
     {
-        name        : 'HTML, CSS, Sass',
-        icons       : [ HtmlIcon, CssIcon, SassIcon ],
-        description : 'In-depth knowledge for creating beautiful, responsive websites with HTML, CSS and SASS.'
+        name: 'HTML, CSS, Sass',
+        icons: [HtmlIcon, CssIcon, SassIcon],
+        description: 'In-depth knowledge for creating beautiful, responsive websites with HTML, CSS and SASS.',
     },
     {
-        name        : 'PHP',
-        icons       : [ PhpIcon ],
-        description : 'Over three years of experience with creating functional back-end websites with PHP.'
+        name: 'PHP',
+        icons: [PhpIcon],
+        description: 'Over three years of experience with creating functional back-end websites with PHP.',
     },
     {
-        name        : 'Ubuntu, Centos',
-        icons       : [ UbuntuIcon, CentosIcon ],
-        description : 'Proficient understanding of how to navigate and work within the Linux CLI.'
+        name: 'Ubuntu, Centos',
+        icons: [UbuntuIcon, CentosIcon],
+        description: 'Proficient understanding of how to navigate and work within the Linux CLI.',
     },
     {
-        name        : 'GitHub',
-        icons       : [ GithubIcon ],
-        description : 'Understanding of the GitHub platform and how to utilize Git to manage version control.'
-    }
+        name: 'GitHub',
+        icons: [GithubIcon],
+        description: 'Understanding of the GitHub platform and how to utilize Git to manage version control.',
+    },
 ];
 
 /*  <div className="py-6">
@@ -53,7 +53,7 @@ export default function Home() {
                         <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
                             <p className="mb-2 text-3xl font-extrabold dark:text-white text-gray-900 tracking-tight sm:text-5xl transition-colors duration-300">Jason Halvorson</p>
                             <h2 className="text-base font-semibold tracking-wider text-jhpurple uppercase">Full-Stack Web Developer / Software Engineer</h2>
-                            <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">I'm a full-stack web developer with a focus on functionality and UI/UX design. Want to know how I may help your project? Check out my project portfolio and online resume.</p>
+                            <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">I'm a full-stack web developer and aspiring software engineer with a focus on functionality and UI/UX design. Want to know how I may help your project? Check out my project portfolio and online resume.</p>
                             <div className="mt-12">
                                 <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
                                     {technologies.map(({ name, icons, description }) => (
