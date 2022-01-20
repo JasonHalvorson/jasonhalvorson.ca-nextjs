cc81a80d55b6ebe9a9262b388a1743b8bd8a2f1f
Jason <47071224+JasonHalvorson@users.noreply.github.com>
2022-01-20 15:54:36 -0800

    Add resume footer, fix some responsiveness

--- pages/resume.js
+++ pages/resume.js
@@ -1,10 +1,11 @@
 import { contact, workExperience, skills, projects, education } from '/resume/resume-data.js';
 import Image from 'next/image';
+import { GithubIcon, LinkedinIcon } from '../utils/icons';
 
 export default function Resume() {
     return (
         <div className="max-w-5xl px-3 lg:px-5 mx-auto">
-            <article className="mx-auto p-10 mb-5 my-5 dark:bg-gray-800 bg-white shadow-jhpurple transition-colors duration-300">
+            <article className="mx-auto p-5 md:p-10 my-5 dark:bg-gray-800 bg-white shadow-jhpurple transition-colors duration-300">
                 <div className="flex flex-col md:flex-row justify-between">
                     <div className="flex items-center space-x-5">
                         <div className="flex-shrink-0">
@@ -18,7 +19,6 @@ export default function Resume() {
                         </div>
                     </div>
                     <div className="grid grid-cols-1 gap-x-4 gap-y-4 sm:grid-cols-2 mt-5 md:mt-0">
-                        {/* TODO: Make responsive */}
                         {contact.map((item) => (
                             <a key={item.type} className={`mt-1 text-sm dark:text-gray-300 text-gray-700  transition-colors duration-300 ${item.link ? 'hover:text-jhpurple dark:hover:text-jhpurple' : ''}`} href={item.link}>
                                 <item.icon className="mr-1 h-6 w-6 inline" /> {item.value}
@@ -36,7 +36,7 @@ export default function Resume() {
                                 <li key={job.id} className="py-5">
                                     <div className="flex space-x-3">
                                         <div className="flex-1 space-y-1">
-                                            <div className="flex items-center justify-between">
+                                            <div className="md:flex md:items-center md:justify-between">
                                                 <h3 className="font-bold dark:text-white text-gray-900 transition-colors duration-300">{job.title}</h3>
                                                 <p className="text-sm dark:text-gray-500 text-gray-400 transition-colors duration-300">
                                                     {job.company} | {job.period}
@@ -104,7 +104,7 @@ export default function Resume() {
                                     {edu.period ? (
                                         <p className="text-sm md:text-right dark:text-gray-400 text-gray-600 transition-colors duration-300">{edu.period}</p>
                                     ) : (
-                                        <p className="text-right">
+                                        <p className="text-left md:text-right">
                                             <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-jhpurple text-white">
                                                 <svg className="-ml-0.5 mr-1.5 h-2 w-2 text-jhgreen" fill="currentColor" viewBox="0 0 8 8">
                                                     <circle cx={4} cy={4} r={3} />
@@ -116,6 +116,31 @@ export default function Resume() {
                                 </li>
                             ))}
                         </ul>
+                    </div>
+                </div>
+                <div>
+                    <hr className="my-5 dark:border-gray-500 border-gray-400 transition-colors duration-300" />
+                    <div className="flex md:flex-row flex-col justify-center md:space-x-8 space-y-4 md:space-y-0 pt-5 dark:text-white text-gray-900 transition-colors duration-300">
+                        <a href="https://github.com/JasonHalvorson" target="_blank" className="hover:text-jhpurple transition-colors duration-300">
+                            <div className="flex">
+                                <div className="mr-4 flex-shrink-0 self-center h-8 w-8">
+                                    <GithubIcon />
+                                </div>
+                                <div>
+                                    <p className="mt-1">github.com/JasonHalvorson</p>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="https://linkedin.com/in/Jason-Halvorson" target="_blank" className="hover:text-jhpurple transition-colors duration-300">
+                            <div className="flex">
+                                <div className="mr-4 flex-shrink-0 self-center h-8 w-8">
+                                    <LinkedinIcon />
+                                </div>
+                                <div>
+                                    <p className="mt-1">linkedin.com/in/Jason-Halvorson</p>
+                                </div>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </article>
