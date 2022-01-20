65d3b22f410158f2fc0dc74db1361c9a74c96359
Jason <47071224+JasonHalvorson@users.noreply.github.com>
2022-01-19 21:45:33 -0800

    Fix #1

--- components/Sidenav/Sidenav.js
+++ components/Sidenav/Sidenav.js
@@ -162,7 +162,7 @@ export default function Sidenav(props) {
                 </div>
             </div>
             <div className="md:pl-64 flex flex-col flex-1">
-                <div className="sticky top-0 z-10 md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 bg-gray-100">
+                <div className="sticky top-0 z-10 md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 dark:bg-gray-800 bg-gray-100 transition-colors duration-300">
                     <button type="button" className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" onClick={() => setSidenavOpen(true)}>
                         <span className="sr-only">Open sidenav</span>
                         <MenuIcon className="h-6 w-6" aria-hidden="true" />
