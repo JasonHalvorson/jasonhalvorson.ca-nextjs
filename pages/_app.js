bdd75b195e9e8ba0eae07ce662e147b021f94d6e
Jason <47071224+JasonHalvorson@users.noreply.github.com>
2022-02-23 22:17:27 -0800

    Add Ackee for analytics, and sharp for image opt.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
+import { useRouter } from 'next/router';
 import Script from 'next/script';
+import useAckee from 'use-ackee';
 
 import 'tailwindcss/tailwind.css';
 
 import Sidenav from '../components/Sidenav/Sidenav';
 
 function MyApp({ Component, pageProps }) {
+    const { pathname } = useRouter();
+
+    useAckee(pathname, {
+        server: 'https://ackee.dev.jasonhalvorson.ca',
+        domainId: '5656a284-92fb-47f8-9b72-4ad312ccf64d',
+    });
+
     return (
         <>
             <Script strategy="beforeInteractive" src="/scripts/darkMode.js" />
